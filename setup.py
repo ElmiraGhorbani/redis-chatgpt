07def498d005f0aa1c17a425c71fccc8ee16c753
ElmiraGhorbani <elmira.ghorbani96@gmail.com>
2023-07-16 12:25:31 +0330

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import find_packages, setup
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 project_urls = {
   'Github': 'https://github.com/ElmiraGhorbani/redis-chatgpt',
@@ -6,12 +10,14 @@ project_urls = {
 
 setup(
     name='redis_chatgpt',
-    version='0.1.0',
+    version='0.1.1',
     author='Elmira Ghorbani',
     description='A user-friendly package designed to save chat history conversation and truncate conversation if user exceeds max token when using OpenAI chat completion.',
     packages=find_packages(),
     install_requires=[
         'redis>=4.6.0'
     ],
-    project_urls=project_urls
+    project_urls=project_urls,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
